ec1b733056360bd374056127b1c2d3e3985fdb79
damiano <damiano@damiano-LIFEBOOK-AH530>
2016-05-16 16:52:09 +0200

    Aggiunte le annotazioni per l'orm

--- siw_project/src/model/Paziente.java
+++ siw_project/src/model/Paziente.java
@@ -2,12 +2,22 @@ package model;
 
 import java.util.List;
 
+import javax.persistence.*;
+
+@Entity
 public class Paziente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
 	private String nome;
+	
 	private String cognome;
+	
 	private String password;
+	
 	private String email;
 	
-	
+	@OneToMany(mappedBy = "paziente")
 	private List<Esame> esami;
 }
