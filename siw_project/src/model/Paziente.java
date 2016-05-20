17cc9c5041c319a8ebb72a3dd1c5ad2f04d0e89e
damiano <damiano@damiano-LIFEBOOK-AH530>
2016-05-20 11:53:48 +0200

    Aggiunta pagina principale

--- siw_project/src/model/Paziente.java
+++ siw_project/src/model/Paziente.java
@@ -21,7 +21,9 @@ public class Paziente {
 	
 	@OneToMany(mappedBy = "paziente")
 	private List<Esame> esami;
+	
 	public Paziente(){}
+	
 	public Paziente(String nome, String cognome, String password, String email) {
 		this.nome = nome;
 		this.cognome = cognome;
@@ -29,41 +31,57 @@ public class Paziente {
 		this.email = email;
 		this.esami = new LinkedList<>();
 	}
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getCognome() {
 		return cognome;
 	}
+	
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public List<Esame> getEsami() {
 		return esami;
 	}
+	
 	public void setEsami(List<Esame> esami) {
 		this.esami = esami;
+	}
+	
+	public boolean checkPassword(String password) {
+		return this.password.equals(password);
 	}
 	
 }
